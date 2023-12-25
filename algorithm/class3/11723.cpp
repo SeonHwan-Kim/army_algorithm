43926a8a077efd3448185367775242cc026476ff
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-12-25 22:39:54 +0900

    add : 11723 해결

--- algorithm/class3/11723.cpp
+++ algorithm/class3/11723.cpp
@@ -1,3 +1,42 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     int M, x, set = 0;
+//     string operation;
+
+//     cin >> M;
+
+//     for(int i = 0; i < M; i++) {
+//         cin >> operation;
+//         if(operation == "add") {
+//             cin >> x;
+//             set |= (1 << x);
+//         } else if(operation == "remove") {
+//             cin >> x;
+//             set &= ~(1 << x);
+//         } else if(operation == "check") {
+//             cin >> x;
+//             if(set & (1 << x)) {
+//                 cout << 1 << "\n";
+//             } else {
+//                 cout << 0 << "\n";
+//             }
+//         } else if(operation == "toggle") {
+//             cin >> x;
+//             set ^= (1 << x);
+//         } else if(operation == "all") {
+//             set = (1 << 21) - 1;
+//         } else {
+//             set = 0;
+//         }
+//     }
+// }
+
 #include <iostream>
 
 using namespace std;
@@ -5,8 +44,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int M, x, set = 0;
+    int M, x;
+    int set[21] = {false,};
     string operation;
 
     cin >> M;
@@ -15,24 +54,28 @@ int main() {
         cin >> operation;
         if(operation == "add") {
             cin >> x;
-            set |= (1 << x);
+            set[x] = true;
         } else if(operation == "remove") {
             cin >> x;
-            set &= ~(1 << x);
+            set[x] = false;
         } else if(operation == "check") {
             cin >> x;
-            if(set & (1 << x)) {
+            if(set[x]) {
                 cout << 1 << "\n";
             } else {
                 cout << 0 << "\n";
             }
         } else if(operation == "toggle") {
             cin >> x;
-            set ^= (1 << x);
+            set[x] = !set[x];
         } else if(operation == "all") {
-            set = (1 << 21) - 1;
+            for(int i = 0; i < 21; i++) {
+                set[i] = true;
+            }
         } else {
-            set = 0;
+            for(int i = 0; i < 21; i++) {
+                set[i] = false;
+            }
         }
     }
 }
